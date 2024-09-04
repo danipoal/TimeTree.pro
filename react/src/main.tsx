a473bd3526eaff35ec039d2a0ed4933524f2290e
Daniel Alvarez <danipoal.0@gmail.com>
2024-09-04 19:58:24 +0200

    check

---
+++ react/src/main.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+
+const ScrollToTop: React.FC = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <Router>
+        <ScrollToTop />
+        <Routes>
+        <Route index element={<Home />} />
+
+        </Routes>
+      </Router>
+    </React.StrictMode>
+  );
+} else {
+  console.error("No se pudo encontrar el elemento raíz en el DOM");
+}
