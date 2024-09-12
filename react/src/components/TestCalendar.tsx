b5a917b05b4efd998cc4d08140f33ed16cf696b0
Daniel Alvarez <danipoal.0@gmail.com>
2024-09-12 18:47:21 +0200

    display events

--- react/src/components/TestCalendar.tsx
+++ react/src/components/TestCalendar.tsx
@@ -2,13 +2,14 @@ import React, { useState } from 'react';
 import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
 import { format, parse, startOfWeek, getDay } from 'date-fns';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import { enUS } from 'date-fns/locale';
+import { enUS, es } from 'date-fns/locale';
+
 
 
 // Si usas date-fns:
 
 const locales = {
-  'en-US': enUS,
+  'es': es,
 };
 
 const localizer = dateFnsLocalizer({
@@ -18,10 +19,29 @@ const localizer = dateFnsLocalizer({
   getDay,
   locales,
 });
+const event = [
+  {
+    title: 'Reunión importante',
+    start: new Date(2024, 0, 12, 10, 0), // 12 de septiembre de 2024, 10:00 AM
+    end: new Date(2024, 0, 12, 11, 0),   // 12 de septiembre de 2024, 11:00 AM
+  },  
+  {
+    title: 'Reunión importante',
+    start: new Date(2024, 8, 12, 10, 0), // 12 de septiembre de 2024, 10:00 AM
+    end: new Date(2024, 8, 12, 11, 0),   // 12 de septiembre de 2024, 11:00 AM
+  },  
+  {
+    title: 'Reunión importante',
+    start: new Date(2024, 8, 13, 10, 0), // 12 de septiembre de 2024, 10:00 AM
+    end: new Date(2024, 8, 13, 16, 0),   // 12 de septiembre de 2024, 11:00 AM
+  }
+];
+
 
 const TestCalendar = () => {
-  const [events, setEvents] = useState([]); // Puedes cargar tus eventos aquí
+  const [events, setEvents] = useState(event); // Puedes cargar tus eventos aquí
   
+  //setEvents(events);
   return (
     <div style={{ height: 500 }}>
       <Calendar
