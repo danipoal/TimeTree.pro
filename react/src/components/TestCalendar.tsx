63b05d811f63299ce477d44a8cef05646581ea2e
Daniel Alvarez <danipoal.0@gmail.com>
2024-09-12 18:06:13 +0200

    display testCalendar

--- react/src/components/TestCalendar.tsx
+++ react/src/components/TestCalendar.tsx
@@ -1,9 +1,38 @@
-function TestCalendar() {
-    return (
+import React, { useState } from 'react';
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import { format, parse, startOfWeek, getDay } from 'date-fns';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { enUS } from 'date-fns/locale';
 
-      <p>a</p>
-    );
-    }
 
-  export default TestCalendar;
+// Si usas date-fns:
+
+const locales = {
+  'en-US': enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const TestCalendar = () => {
+  const [events, setEvents] = useState([]); // Puedes cargar tus eventos aquí
   
+  return (
+    <div style={{ height: 500 }}>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
+    </div>
+  );
+};
+
+export default TestCalendar;
