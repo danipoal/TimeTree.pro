f6bc6ad5375d711423ef9bf5e0616148a4487b79
Daniel Alvarez <danipoal.0@gmail.com>
2024-09-24 23:13:45 +0200

    Modal para eventos preset

--- react/src/components/TestCalendar.tsx
+++ react/src/components/TestCalendar.tsx
@@ -31,16 +31,33 @@ const event = [
     end: new Date(2024, 8, 12, 11, 0),   // 12 de septiembre de 2024, 11:00 AM
   },  
   {
-    title: 'Reunión importante',
+    title: 'Reunión imporstante',
     start: new Date(2024, 8, 13, 10, 0), // 12 de septiembre de 2024, 10:00 AM
-    end: new Date(2024, 8, 13, 16, 0),   // 12 de septiembre de 2024, 11:00 AM
+    end: new Date(2024, 8, 13, 16, 0),   // 12 de septiembre de 2024, 11:00 AM,
+    allDay: false,
+    resource: { location: "Sala de conferencias", attendees: 5 },
+    desc: "Discusión sobre el nuevo proyecto",
+    url: "https://miempresa.com/eventos/reunion",
+    className: "evento-personalizado",
+    style: { backgroundColor: 'blue', color: 'white' },
   }
 ];
 
 
 const TestCalendar = () => {
   const [events, setEvents] = useState(event); // Puedes cargar tus eventos aquí
-  
+  const [selectedEvent, setSelectedEvent] = useState(null); // Estado para el evento seleccionado
+
+  // Manejador del evento cuando se selecciona un evento
+  const handleSelectEvent = (event) => {
+    setSelectedEvent(event);
+    // Abre el modal
+    const modal = document.getElementById('my_modal_2') as HTMLDialogElement;
+    if (modal) {
+      modal.showModal();
+    }
+  };
+
   //setEvents(events);
   return (
     <div style={{ height: 500 }}>
@@ -50,8 +67,24 @@ const TestCalendar = () => {
         startAccessor="start"
         endAccessor="end"
         style={{ height: 500 }}
+        onSelectEvent={handleSelectEvent} // Llama al evento cuando se hace clic en uno
       />
+
+      {/* MODAL de eventos*/}
+        <button className="btn" 
+          onClick={() => {}}>
+          open modal</button>
+        <dialog id="my_modal_2" className="modal">
+          <div className="modal-box">
+            <h3 className="font-bold text-lg">Hello! {selectedEvent?.desc}</h3>
+            <p className="py-4">Press ESC key or click outside to close</p>
+          </div>
+          <form method="dialog" className="modal-backdrop">
+            <button>close</button>
+          </form>
+        </dialog>
     </div>
+
   );
 };
 
